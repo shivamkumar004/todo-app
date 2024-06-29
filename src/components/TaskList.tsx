a7ebac093410c27c84aba3d72f7c0ca874423eee
shivamkumar004 <shivamkumar91284@gmail.com>
2024-06-29 18:09:31 +0530

    task completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './TaskList.scss'
 
 const TaskList: React.FC= () => {
   return (
-    <div>TaskList</div>
+    <>
+    <div className="task_list_section">
+      <div className="task_list_item">
+      <input type='checkbox' title ='check' className="task_checkbox"></input>
+
+ <input type='text' title ='input' className="task_input"></input>
+< button type ='button' title='summit'>Remove</button>
+      </div>
+
+      
+    </div>
+    
+    
+    
+    </>
   )
 }
 
