90547a79d7ff3114d3b271731773a1713658ead1
shivamkumar004 <shivamkumar91284@gmail.com>
2024-07-01 13:26:14 +0530

    textpage created

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './TaskList.scss'
+import { IInputField } from '../interface/toDo';
 
 const TaskList: React.FC= () => {
+  const[inputFields, setInputFields]=useState<IInputField[]>([]);
+  useEffect(()=>{
+    setInputFields([{value:''}]
+    
+    )
+  },[])
+
+  const handleAddField =()=> {
+    if (inputFields[inputFields.length-1].value.trim()!==''){
+      setInputFields([...inputFields,{value:''}])
+    }
+   
+  
+
+    }
+
+    const handleChange=(index:number,
+      event:React.ChangeEvent<HTMLInputElement>
+      )=>{
+     
+      let value =[...inputFields];
+      value[index].value= event.target.value;
+      setInputFields(value);
+      localStorage.setItem('tasklist',JSON.stringify(value))
+      handleAddField();
+   
+  }
   return (
     <>
     <div className="task_list_section">
-      <div className="task_list_item">
+
+      {inputFields.map((item,index)=>(
+      <div className="task_list_item" key={index}>
       <input type='checkbox' title ='check' className="task_checkbox"></input>
 
- <input type='text' title ='input' className="task_input"></input>
-< button type ='button' title='summit'>Remove</button>
+       <input type='text' title ='input' className="task_input" value={item.value} onChange={(event)=>{
+        handleChange(index,event)
+       }}></input>
+      < button type ='button' title='summit'>Remove</button>
       </div>
+      ))}
+      
 
       
     </div>
